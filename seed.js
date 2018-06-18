1d4df679cb6b8170e8accc462dc54a4dc3752ba0
Victor Li <vli8@fordham.edu>
2018-06-18 18:03:18 -0400

    Done

--- seed.js
+++ seed.js
@@ -2,11 +2,22 @@ const {db, Gardener, Plot, Vegetable} = require('./models');
 
 db.sync({force: true}).then(() => {
   console.log('Success');
-}).then(() => { Vegetable.bulkCreate([
+})
+.then(() => { return Vegetable.bulkCreate([
     {name: 'carrot', color: 'purple', planted_on: new Date()},
     {name: 'tomato', color: 'green', planted_on: new Date()}
-])
-}).catch((error) => {
+    ])
+})
+.then((veggies) => {
+      veggies.forEach((elem)=>{
+        Gardener.create({
+            name : "Jessica",
+            age: 19,
+            favoriteVegetableid : elem.id
+        })
+      })
+})
+.catch((error) => {
   console.log(error);
   db.close();
 });
